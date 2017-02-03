3d51a5ec8bb4c844f2b14ce6d25dfde3d5c0c4fb
pwcong <pwcong@foxmail.com>
2017-02-03 20:55:04 +0800

    [+] 完成用户登陆注册界面

--- src/main/web/src/index.js
+++ src/main/web/src/index.js
@@ -1,12 +1,18 @@
-/**
- * Created by Pwcong on 2017/1/29.
- */
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { createStore, applyMiddleware } from 'redux' 
+import thunk from 'redux-thunk'
+import { Provider } from 'react-redux'
+import { Router, browserHistory } from 'react-router'
+
+import reducer from './reducer'
+import routes from './routes'
+
+const store = createStore(reducer,applyMiddleware(thunk))
 
 ReactDOM.render(
-    <div>
-        Hello Pwcong
-    </div>,
-    document.getElementById('app')
+	<Provider store={store}>
+		<Router history={browserHistory} routes={routes}/>
+	</Provider>,
+	document.getElementById('app')
 )
